b8d9e075796dc187a3fae379c9eeb61f53e8fc3a
unknown <Jose Salinas>
2020-06-14 03:34:31 -0700

    add main

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "null_resource" "depends_on" {
+    triggers = {
+        depends_on = join("", var.db_instance_depends_on)
+    }
+}
+
+resource "aws_  " "db_instance_terraform" {
+    count                 = var.db_instance_count
+    depends_on            = [null_resource.depends_on]
+    allocated_storage     = var.db_instance_allocated_storage
+    max_allocated_storage = var.db_instance_max_allocated_storage
+    storage_type          = var.db_instance_storage_type
+    engine                = var.db_instance_engine
+    engine_version        = var.db_instance_engine_version
+    instance_class        = var.db_instance_instance_class
+    name                  = var.db_instance_name
+    username              = var.db_instance_username
+    password              = var.db_instance_password
+    parameter_group_name  = var.db_instance_parameter_group_name
+    multi_az              = var.db_instance_multi_az
+}
